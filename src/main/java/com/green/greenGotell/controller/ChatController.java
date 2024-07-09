80f4e1d097d3de951eb817177db1d58d3f370955
Yujin <Administrator@304-12>
2024-07-09 16:34:29 +0900

    feat : Chat 기능 완료 테스트 기능 최종완료됨

--- src/main/java/com/green/greenGotell/controller/ChatController.java
+++ src/main/java/com/green/greenGotell/controller/ChatController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 
 @Controller
@@ -22,6 +25,12 @@ public class ChatController {
 	@GetMapping("/caht/{no}")
 	public String detail(@PathVariable("no")long no) {
 		return "/views/test/detail";
+	}
+	
+	@PutMapping("/chat/{no}")
+	public String update(@PathVariable ("no")long no) {
+		
+		return "redirect:/chat";
 	}
 	
 }
