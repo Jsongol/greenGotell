42dff9db04d6445340209f768633f3d229fdbf6d
dohyun <dohyun@304-10>
2024-07-09 16:34:26 +0900

    feat : 테스트 기능 완료!!!
    테스트 기능 최종완료됨

--- src/main/java/com/green/greenGotell/controller/InventoryController.java
+++ src/main/java/com/green/greenGotell/controller/InventoryController.java
@@ -3,6 +3,7 @@ package com.green.greenGotell.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 @Controller
@@ -15,6 +16,10 @@ public class InventoryController {
 	@GetMapping("/inventory/{no}")
 	public String detail(@PathVariable("no") long no) {
 		return "/views/test/detail";
+	}
+	@PutMapping("/inventory/{no}")
+	public String update(@PathVariable("no") long no) {
+		return "redirect:/test";
 	}
 	
 }
