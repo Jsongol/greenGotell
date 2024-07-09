5e0646a1d4d04dcf6530a2561d76b327135c4dcd
gon <Administrator@host.docker.internal>
2024-07-09 16:11:50 +0900

    feat : notice 상세페이지까지 작업 완료
    공지사항 상세페이지까지만 만들고 아직 미완성

--- src/main/java/com/green/greenGotell/controller/NoticesController.java
+++ src/main/java/com/green/greenGotell/controller/NoticesController.java
@@ -2,6 +2,9 @@ package com.green.greenGotell.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class NoticesController {
@@ -9,8 +12,13 @@ public class NoticesController {
 	@GetMapping("/notices")
 	public String list() {
 		
-		
 		return "/views/notices/list";
 	}
+	
+	@GetMapping("/notices/{no}")
+	public String detail(@PathVariable("no") long no) {
+		return "/views/notices/detail";
+	}
+	
 
 }
