b514a557d81c542fbca1a430648d43a1f298643e
Yujin <eee111eee134@gmail.com>
2024-07-11 14:53:06 +0900

    feat:chat list

--- src/main/java/com/green/greenGotell/chatbot/BotController.java
+++ src/main/java/com/green/greenGotell/chatbot/BotController.java
@@ -1,20 +1,33 @@
 package com.green.greenGotell.chatbot;
 
+import java.text.MessageFormat;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.util.HtmlUtils;
+
+import lombok.RequiredArgsConstructor;
 
 
 
 @Controller
 public class BotController {
 
-	@GetMapping("/chat")
+	@GetMapping("/chatbot")
 	public String list() {
-		return "/views/chat/index";
+		return "/views/chatbot/list";
 	}
 	
+	
+	@MessageMapping("/hello")
+	@SendTo("/topic/Goting")
+	public Goting greeting(HelloMessage message) throws Exception {
+		System.out.println("클라이언트에서 이름을을 객체로 보냈어요>>>>"+message.getName());
+		return new Goting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
+	}
 	
 }
--- src/main/java/com/green/greenGotell/chatbot/Goting.java
+++ src/main/java/com/green/greenGotell/chatbot/Goting.java
@@ -1,9 +1,11 @@
 package com.green.greenGotell.chatbot;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class Goting {
-
+	
 	private String content;
 }
