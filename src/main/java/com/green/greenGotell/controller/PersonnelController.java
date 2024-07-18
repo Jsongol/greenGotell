7eda174f3cee5dea9f99b722ff8ba7497c5042b0
songol <thdgnl200@gmail.com>
2024-07-18 15:59:37 +0900

    feat:인사관리 직원추가 작업중

--- src/main/java/com/green/greenGotell/controller/PersonnelController.java
+++ src/main/java/com/green/greenGotell/controller/PersonnelController.java
@@ -4,19 +4,29 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import com.green.greenGotell.domain.dto.CreateEmployeeDTO;
+import com.green.greenGotell.service.PersonnelService;
+
+import lombok.RequiredArgsConstructor;
 
 
+@RequiredArgsConstructor
 @Controller
 public class PersonnelController {
+	
+	private final PersonnelService personnelService;
 
 	@GetMapping("/personnel")
 	public String list() {
 		return "/views/personnel/employee/list";
 	}
 	
-	@PostMapping("/personnel")
-	public String write() {
-		return new String();
+	@PostMapping("/personnel/create")
+	public String create(CreateEmployeeDTO dto) {
+		System.out.println(dto.getPass());
+		personnelService.createEmployee(dto);
+		
+		return "redirect:/personnel";
 	}
 	
 	
---
+++ src/main/java/com/green/greenGotell/service/PersonnelService.java
@@ -0,0 +1,9 @@
+package com.green.greenGotell.service;
+
+import com.green.greenGotell.domain.dto.CreateEmployeeDTO;
+
+public interface PersonnelService{
+
+	void createEmployee(CreateEmployeeDTO dto);
+
+}
