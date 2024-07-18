6649b2306bfbdc320ca7317c78024ca0147f73e4
gon <men1032415@gmail.com>
2024-07-18 15:48:18 +0900

    feat merge 0718

---
+++ src/main/java/com/green/greenGotell/domain/dto/NoticeListDTO.java
@@ -0,0 +1,18 @@
+package com.green.greenGotell.domain.dto;
+
+import java.time.LocalDateTime;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class NoticeListDTO{
+	
+	private long no;
+	private String title;
+	private String division;	//구분-"전체","영화관"
+	private boolean fixed;		//고정여부
+	private LocalDateTime updatedAt;
+
+}
--- src/main/java/com/green/greenGotell/service/impl/NoticeServiceProcess.java
+++ src/main/java/com/green/greenGotell/service/impl/NoticeServiceProcess.java
@@ -1,14 +1,24 @@
 package com.green.greenGotell.service.impl;
 
+import java.util.stream.Collectors;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
 import com.green.greenGotell.domain.dto.NoticeSaveDTO;
+import com.green.greenGotell.domain.entity.NoticeEntity;
 import com.green.greenGotell.domain.repository.NoticeEntityRepository;
 import com.green.greenGotell.service.NoticeService;
 
 import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 
+@ToString
 @RequiredArgsConstructor
 @Service
 public class NoticeServiceProcess implements NoticeService{
@@ -16,10 +26,24 @@ public class NoticeServiceProcess implements NoticeService{
 	private final NoticeEntityRepository repository;
 
 	@Override
-	public void listProcess(int division, Model model) {
+	public void listProcess(int _division, Model model) {
+		int pageNumber=1;//1페이지
+		int pageSize=10; //최대 10개까지
+		Sort sort=Sort.by(Direction.DESC, "fixed","no");
+		Pageable pageable=PageRequest.of(pageNumber-1, pageSize, sort);
 		
+		String division = (_division==1)? "전체":"2";
+		
+		//JPA 쿼리메서드 : findAll()
+		//사용자가 만들수 있는 쿼리메서드 문법-키워드
+		Page<NoticeEntity> result=repository.findAllByDivision(division,pageable);
+		System.out.println(">>>>"+result.getContent());
+		model.addAttribute("list", result.getContent().stream()
+										.map(NoticeEntity::toNoticeListDTO)//메서드 참조를 사용할 수 있다
+										.collect(Collectors.toList()));	
 		
 	}
+	
 
 	@Override
 	public void saveProcess(NoticeSaveDTO dto) {
